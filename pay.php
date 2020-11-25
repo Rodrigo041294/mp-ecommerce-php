d4ce3d9caeba6a8944dc5dd6594a92c2907f5d26
Rodrigo Eduardo Rodriguez Araiza <reraitc@gmail.com>
2020-11-25 09:31:33 -0700

    add fonts and mercado pago sdk

---
+++ pay.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'vendor/autoload.php';
+MercadoPago\SDK::setAccessToken('TEST-4818673285371487-112515-961f0fd15bcbb99e0182b0409c088ea5-176213646');
+
+$payment = new MercadoPago\Payment();
+$payment->transaction_amount = 141;
+$payment->token = "YOUR_CARD_TOKEN";
+$payment->description = "Ergonomic Silk Shirt";
+$payment->installments = 1;
+$payment->payment_method_id = "visa";
+$payment->payer = array(
+    "email" => "larue.nienow@email.com"
+);
+
+$payment->save();
+
+echo $payment->status;
+?>
